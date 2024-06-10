70bd8fb93d032012fef8fcf47a0c9025564cce2c
Kevelly Ribeiro <142060005+KevellyRibeiro@users.noreply.github.com>
2024-06-10 20:19:02 -0300

    çlkçl

--- LeticiaCristovão/Enamorado/src/Router.jsx
+++ LeticiaCristovão/Enamorado/src/Router.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import {Home}  from "../../Home/Home.jsx"; 
+import {Home}  from ".Home.jsx"; 
 
 export function Router() {
   return (
